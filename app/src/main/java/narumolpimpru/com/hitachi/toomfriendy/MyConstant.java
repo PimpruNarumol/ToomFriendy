f7be5324b4ff175f85b9ca8431bc7907f2474508
narumol.toom <narumol@gmail.com>
2018-12-04 13:43:43 +0700

    Get Json Finish

--- app/src/main/java/narumolpimpru/com/hitachi/toomfriendy/MyConstant.java
+++ app/src/main/java/narumolpimpru/com/hitachi/toomfriendy/MyConstant.java
@@ -3,7 +3,12 @@ package narumolpimpru.com.hitachi.toomfriendy;
 public class MyConstant {
 
     private String urlAddUser = "http://www.androidthai.in.th/hit/addUserToom.php";
-    private  String urlPreAvatar ="http://androidthai.in.th/hit/NarumolPimpru";
+    private String urlPreAvatar ="http://androidthai.in.th/hit/NarumolPimpru";
+    private String urlGetAllUser = "http://www.androidthai.in.th/hit/getAllUserToom.php";
+
+    public String getUrlGetAllUser() {
+        return urlGetAllUser;
+    }
 
     public String getUrlPreAvatar() {
         return urlPreAvatar;
